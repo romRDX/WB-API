18d1d2dfaff281fe61388fb005a2c4418eb29076
Rom. D <romullo_sas@outlook.com>
2022-10-03 18:26:18 -0300

    fix - separate api

--- index.js
+++ index.js
@@ -15,33 +15,33 @@ const server = app.listen(port, () => {
 
 // daqui para baixo
 
-const express = require('express');
-const PORT = process.env.PORT || 3005;
+// const express = require('express');
+// const PORT = process.env.PORT || 3005;
 
-function onMessage(ws, data) {
+// function onMessage(ws, data) {
 
-    const parsedData = JSON.parse(data);
+//     const parsedData = JSON.parse(data);
 
-    // if(parsedData.eventType === "pve"){
-    //     pveHandler(ws, parsedData);
-    // } 
+//     // if(parsedData.eventType === "pve"){
+//     //     pveHandler(ws, parsedData);
+//     // } 
 
-    // if(parsedData.eventType === "pvp"){
-    //     pvpHandler(ws, parsedData);
-    // } 
-    console.log("XX: ", parsedData);
-    ws.send(`recebido!`);
-}
+//     // if(parsedData.eventType === "pvp"){
+//     //     pvpHandler(ws, parsedData);
+//     // } 
+//     console.log("XX: ", parsedData);
+//     ws.send(`recebido!`);
+// }
 
-const wsServer = express().listen(PORT, () => console.log(`Listening on ${PORT}`));
+// const wsServer = express().listen(PORT, () => console.log(`Listening on ${PORT}`));
 
-const { Server } = require('ws');
+// const { Server } = require('ws');
 
-const wss = new Server({ wsServer });
+// const wss = new Server({ wsServer });
 
-wss.on('connection', (ws) => {
-    console.log('Client connected');
-    ws.on('message', data => onMessage(ws, data));
-    ws.on('close', () => console.log('Client disconnected'));
-});
+// wss.on('connection', (ws) => {
+//     console.log('Client connected');
+//     ws.on('message', data => onMessage(ws, data));
+//     ws.on('close', () => console.log('Client disconnected'));
+// });
 
