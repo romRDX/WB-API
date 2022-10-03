6ced31c76af46bcf2aa5ddfa7b7e90ec75c4ba71
Rom. D <romullo_sas@outlook.com>
2022-10-03 18:01:16 -0300

    try adding ws to heroku

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const app = require('./src/app');
-const appWs = require('./src/app-ws');
+// const appWs = require('./src/app-ws');
 
 const port = process.env.PORT || 3001;
  
@@ -7,8 +7,45 @@ const server = app.listen(port, () => {
     console.log(`App Express is running!`);
 })
 
-const wss = appWs(server);
+// const wss = appWs(server);
 
 // setInterval(() => {
 //     wss.broadcast({ n: Math.random()});
 // }, 2000)
+
+// daqui para baixo
+
+const express = require('express');
+const PORT = process.env.PORT || 3005;
+const INDEX = '/index.html';
+
+function onMessage(ws, data) {
+
+    const parsedData = JSON.parse(data);
+
+    // if(parsedData.eventType === "pve"){
+    //     pveHandler(ws, parsedData);
+    // } 
+
+    // if(parsedData.eventType === "pvp"){
+    //     pvpHandler(ws, parsedData);
+    // } 
+    console.log("XX: ", parsedData);
+    ws.send(`recebido!`);
+}
+
+const wsServer = express()
+  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+
+  const { Server } = require('ws');
+
+  const wss = new Server({ wsServer });
+
+  wss.on('connection', (ws) => {
+    console.log('Client connected');
+    ws.on('message', data => onMessage(ws, data));
+    ws.on('close', () => console.log('Client disconnected'));
+  });
+
